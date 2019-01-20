897cb1e196f21d5c8987cad77c0d1e4b9dbfef3b
JLJ <j.r.lloydjones@gmail.com>
2019-01-20 11:16:29 +0000

    Refactor #calculate_answer using private methods

--- lib/game.rb
+++ lib/game.rb
@@ -5,6 +5,17 @@ class Game
   end
 
   def calculate_answer(guess)
+    correct_guess?(guess)
+    partial_guess(guess)
+  end
+
+  private
+
+  def correct_guess?(guess)
+    return @word if (guess == @word)
+  end
+
+  def partial_guess(guess)
     result = []
     @word.chars.each_with_index do |letter, index|
       letter == guess[index] ? result.push(letter) : result.push('_')
